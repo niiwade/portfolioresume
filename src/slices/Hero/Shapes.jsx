a3ea3019d70aeb2e739652f17e362035b37b19f0
niiwade <niilantewade77@gmail.com>
2024-01-10 05:00:03 +0000

    fixed on shapes compoenent

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -21,21 +21,7 @@ export default function Shapes() {
     return (
         <div className="row-span-1 row-start-1 -mt-9 aspect-square md:col-span-1 md:col-start-2 md:mt-0">
 
-            {/* <div>
-          <web.main  style={{ background: props.open.to([0, 1], ['#f0f0f0', '#d25578']) }}>
-                <web.h1 style={{ opacity: props.open.to([0, 1], [1, 0]), transform: props.open.to((o) => `translate3d(-50%,${o * 50 - 100}px,0)`) }}>click</web.h1>
-                <Canvas dpr={[1, 2]} camera={{ position: [0, 0, -30], fov: 35 }}>
-                    <three.pointLight position={[10, 10, 10]} intensity={1.5} color={props.open.to([0, 1], ['#f0f0f0', '#d25578'])} />
-                    <Suspense fallback={null}>
-                        <group rotation={[0, Math.PI, 0]} onClick={(e) => (e.stopPropagation(), setOpen(!open))}>
-                            <Model open={open} hinge={props.open.to([0, 1], [1.575, -0.425])} />
-                        </group>
-                        <Environment preset="city" />
-                    </Suspense>
-                    <ContactShadows position={[0, -4.5, 0]} opacity={0.4} scale={20} blur={1.75} far={4.5} />
-                </Canvas>
-            </web.main>
-          </div> */}
+        
             <Canvas className="z-0" shadows gl={{ antialias: false }} dpr={[1, 1.5]} camera={{
                 position: [0, 0, 25],
                 fov: 30, near: 1, far: 40
